297f1da38e57d0a391b437072d9288603bed9626
Yizi Gu <yizigu@gmail.com>
2019-09-03 12:13:39 -0500

    [refactor][buckify]make the class modulelize

---
+++ src/InstrumentClient/InstrumentClient.cpp
@@ -0,0 +1,26 @@
+#include "InstrumentClient.h"
+#include <glog/logging.h>
+
+
+
+using namespace Dyninst;
+using namespace romp;
+using namespace std;
+
+InstrumentClient::InstrumentClient(const string& programName, shared_ptr<BPatch> bpatchPtr) {
+  LOG(INFO) << "constructor of InstrumentClient";
+  bpatchPtr_ = move(bpatchPtr);
+  addrSpacePtr_ = initInstrumenter(programName);
+  LOG(INFO) << "init instrumentation success";
+}
+
+unique_ptr<BPatch_addressSpace> 
+InstrumentClient::initInstrumenter(const string& programName) {
+  LOG(INFO) << "start init instrumentation: " << programName.c_str();
+  auto handle = bpatchPtr_->openBinary(programName.c_str(), true);
+  if (!handle) {
+    LOG(FATAL) << "cannot open binary: " << programName;    
+  }
+  unique_ptr<BPatch_addressSpace> ptr(handle);  
+  return ptr;
+}
---
+++ src/InstrumentClient/InstrumentClient.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "InstrumentClient.h"
+
+#include <memory>
+#include <string>
+
+#include "BPatch.h"
+#include "BPatch_addressSpace.h"
+#include "BPatch_process.h"
+
+namespace romp {
+  class InstrumentClient {
+    public:
+      InstrumentClient(const std::string& programName, std::shared_ptr<BPatch> bpatchPtr);
+    private:
+      std::unique_ptr<BPatch_addressSpace> initInstrumenter(const std::string& programName); 
+    private:    
+      std::unique_ptr<BPatch_addressSpace> addrSpacePtr_;
+      std::shared_ptr<BPatch> bpatchPtr_;
+  };
+}
