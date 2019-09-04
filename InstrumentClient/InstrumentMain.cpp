37d5cd9113ced014bb8825d45d823c247a5641f5
Yizi Gu <yizigu@gmail.com>
2019-09-03 23:58:35 -0500

    [spack-build] add gflags

--- InstrumentClient/InstrumentMain.cpp
+++ InstrumentClient/InstrumentMain.cpp
@@ -1,3 +1,4 @@
+#include <gflags/gflags.h>
 #include <glog/logging.h>
 #include "BPatch.h"
 #include "BPatch_addressSpace.h"
@@ -5,9 +6,12 @@
 using namespace std;
 using namespace Dyninst;
 
+DEFINE_string(program, "", "program to be instrumented");
+
 int main(int argc, char* argv[]) {
   BPatch_addressSpace* app;
   google::InitGoogleLogging(argv[0]);
-  LOG(FATAL) << "TEST TEST ";
+  FLAGS_alsologtostderr = 1;
+  LOG(INFO) << "TEST TEST ";
   return 0;
 }
