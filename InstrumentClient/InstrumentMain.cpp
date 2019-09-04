1298cb82276ce4198156f8cd7c16762af54ecf94
Yizi Gu <yizigu@gmail.com>
2019-09-04 11:24:16 -0500

    [refactor]init client in main

--- InstrumentClient/InstrumentMain.cpp
+++ InstrumentClient/InstrumentMain.cpp
@@ -1,17 +1,23 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
+#include <memory>
 #include "BPatch.h"
 #include "BPatch_addressSpace.h"
 #include "BPatch_process.h"
-using namespace std;
 using namespace Dyninst;
+using namespace romp;
+using namespace std;
 
 DEFINE_string(program, "", "program to be instrumented");
 
 int main(int argc, char* argv[]) {
-  BPatch_addressSpace* app;
   google::InitGoogleLogging(argv[0]);
   FLAGS_alsologtostderr = 1;
+  if (FLAGS_program == "") {
+    LOG(FATAL) << "no program name specified";
+  } 
+  auto bpatchPtr = make_shared<BPatch>(); 
+  unique_ptr<InstrumentClient> client(new InstrumentClient(FLAGS_program, bpatchPtr));
   LOG(INFO) << "TEST TEST ";
   return 0;
 }
