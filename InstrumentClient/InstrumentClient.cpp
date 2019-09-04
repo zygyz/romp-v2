924b0e330149a454c4c06933c49003a197d17b5e
Yizi Gu <yizigu@gmail.com>
2019-09-03 22:07:35 -0500

    [experimental][spack-build]test build instrument client

--- InstrumentClient/InstrumentClient.cpp
+++ InstrumentClient/InstrumentClient.cpp
@@ -1,5 +1,5 @@
 #include "InstrumentClient.h"
-#include <glog/logging.h>
+//#include <glog/logging.h>
 
 
 
@@ -8,18 +8,18 @@ using namespace romp;
 using namespace std;
 
 InstrumentClient::InstrumentClient(const string& programName, shared_ptr<BPatch> bpatchPtr) {
-  LOG(INFO) << "constructor of InstrumentClient";
+ // LOG(INFO) << "constructor of InstrumentClient";
   bpatchPtr_ = move(bpatchPtr);
   addrSpacePtr_ = initInstrumenter(programName);
-  LOG(INFO) << "init instrumentation success";
+ // LOG(INFO) << "init instrumentation success";
 }
 
 unique_ptr<BPatch_addressSpace> 
 InstrumentClient::initInstrumenter(const string& programName) {
-  LOG(INFO) << "start init instrumentation: " << programName.c_str();
+ // LOG(INFO) << "start init instrumentation: " << programName.c_str();
   auto handle = bpatchPtr_->openBinary(programName.c_str(), true);
   if (!handle) {
-    LOG(FATAL) << "cannot open binary: " << programName;    
+//    LOG(FATAL) << "cannot open binary: " << programName;    
   }
   unique_ptr<BPatch_addressSpace> ptr(handle);  
   return ptr;
--- InstrumentClient/InstrumentMain.cpp
+++ InstrumentClient/InstrumentMain.cpp
@@ -1,4 +1,4 @@
-#include <glog/logging.h>
+//#include <glog/logging.h>
 #include "BPatch.h"
 #include "BPatch_addressSpace.h"
 #include "BPatch_process.h"
@@ -7,7 +7,7 @@ using namespace Dyninst;
 
 int main(int argc, const char* argv[]) {
   BPatch_addressSpace* app;
-  google::InitGoogleLogging(argv[0]);
-  LOG(INFO) << "TEST TEST ";
+ // google::InitGoogleLogging(argv[0]);
+ // LOG(INFO) << "TEST TEST ";
   return 0;
 }
