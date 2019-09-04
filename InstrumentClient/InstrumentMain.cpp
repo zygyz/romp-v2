a3e4fd8eb6fb28e6b6c2c2e891b6d6ae96bca91c
Yizi Gu <yizigu@gmail.com>
2019-09-03 23:19:59 -0500

    [experimental][spack-build] test glog dependency

--- InstrumentClient/InstrumentMain.cpp
+++ InstrumentClient/InstrumentMain.cpp
@@ -1,4 +1,4 @@
-#include <folly/logging/xlog.h>
+#include <glog/logging.h>
 #include "BPatch.h"
 #include "BPatch_addressSpace.h"
 #include "BPatch_process.h"
@@ -7,8 +7,7 @@ using namespace Dyninst;
 
 int main(int argc, const char* argv[]) {
   BPatch_addressSpace* app;
-  //google::InitGoogleLogging(argv[0]);
-  //LOG(INFO) << "TEST TEST ";
-  XLOG(INFO) << "test test ";
+  google::InitGoogleLogging(argv[0]);
+  LOG(INFO) << "TEST TEST ";
   return 0;
 }
