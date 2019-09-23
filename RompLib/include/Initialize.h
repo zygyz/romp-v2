c8e8bd13dd87135411ffcf26495638f7172477ea
Yizi Gu <yizigu@gmail.com>
2019-09-23 17:17:33 -0500

    [refactor][romp-lib] put ompt_start_tool in cpp file

--- RompLib/include/Initialize.h
+++ RompLib/include/Initialize.h
@@ -3,12 +3,11 @@
 #include <glog/logging.h>
 #include <ompt.h>
 
-namespace romp{
-
 /* This header file defines functions that are used 
  * to initialize OMPT interface. 
- */
-extern "C" {
+*/
+namespace romp{
+
 /** 
  *  initialize OMPT interface by registering callback functions
  */
@@ -27,18 +26,6 @@ void omptFinalize(ompt_data_t* toolData) {
   LOG(INFO) << "finalizing ompt";
 }
 
-/** 
- * implement ompt_start_tool which is defined in OpenMP spec 5.0
- */
-ompt_start_tool_result_t* ompt_start_tool(
-        unsigned int ompVersion,
-        const char* runtimeVersion) {
-  ompt_data_t data;
-  static ompt_start_tool_result_t startToolResult = { 
-      &omptInitialize, &omptFinalize, data}; 
-  LOG(INFO) << "ompt_start_tool";
-  return &startToolResult;
-}
 
 }
 
