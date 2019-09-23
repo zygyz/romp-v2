eea9d1a8f265d00bd86cba2525e3e5ed02fb6839
Yizi Gu <yizigu@gmail.com>
2019-09-23 12:05:53 -0500

    [refactor][romp-lib] move initialize to header file

--- RompLib/src/RompLib.cpp
+++ RompLib/src/RompLib.cpp
@@ -1,8 +1,4 @@
-//#include "Initialize.h"
-
-#include <glog/logging.h>
-#include <ompt.h>
-
+#include "Initialize.h"
 
 using namespace std;
 
@@ -12,28 +8,6 @@ namespace romp {
 
 extern "C" {
 
-int omptInitialize(ompt_function_lookup_t functionLookup,
-                   int initialDeviceNum,
-                   ompt_data_t* toolData) {
-  google::InitGoogleLogging("romp");
-  LOG(INFO) << "start initializing ompt";      
-
-}
-
-void omptFinalize(ompt_data_t* toolData) {
-  LOG(INFO) << "finalizing ompt";
-}
-
-ompt_start_tool_result_t* ompt_start_tool(
-        unsigned int ompVersion,
-        const char* runtimeVersion) {
-  ompt_data_t data;
-  static ompt_start_tool_result_t startToolResult = { 
-      &omptInitialize, &omptFinalize, data}; 
-  LOG(INFO) << "ompt_start_tool";
-  return &startToolResult;
-}
-
 void 
 checkAccess(void* address,
             uint32_t bytesAccessed,
@@ -41,13 +15,11 @@ checkAccess(void* address,
             bool hwLock,
             bool isWrite) {
    
-    /*
  LOG(INFO) << "address: " << address 
            << " bytesAccessed: " << bytesAccessed 
            << " instnAddr: " << instnAddr
            << " hwlock: " << hwLock
            << " isWrite: " << isWrite;
-           */
 
 }
 
