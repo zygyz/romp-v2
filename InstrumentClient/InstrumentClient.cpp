9334b19f376a39170fc6fec935745d65fc116c9c
Yizi Gu <yizigu@gmail.com>
2019-09-23 15:16:07 -0500

    [bug-fix] fix typo

--- InstrumentClient/InstrumentClient.cpp
+++ InstrumentClient/InstrumentClient.cpp
@@ -59,7 +59,7 @@ InstrumentClient::getCheckAccessFuncs(
      LOG(FATAL) << "cannot find function `checkAccess` in romp lib";
   }
   vector<BPatch_function*> omptStartToolFuncs;
-  appImage->findFunction("ompt_start_tool", omptStartFuncs);
+  appImage->findFunction("ompt_start_tool", omptStartToolFuncs);
   if (omptStartToolFuncs.size() == 0) {
      LOG(FATAL) << "cannot find function `ompt_start_tool` in romp lib";
   }
