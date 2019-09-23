d1779ddaaefefe98643c0ba23b73c9759f52ac36
Yizi Gu <yizigu@gmail.com>
2019-09-23 14:34:53 -0500

    [refactor][debug] move ompt_start_tool to cpp

--- RompLib/src/RompLib.cpp
+++ RompLib/src/RompLib.cpp
@@ -1,8 +1,38 @@
-#include "Initialize.h"
+//#include "Initialize.h"
 
+#include <glog/logging.h>
+#include <ompt.h>
 using namespace std;
 
 namespace romp {
+
+int omptInitialize(ompt_function_lookup_t functionLookup,
+                   int initialDeviceNum,
+                   ompt_data_t* toolData) {
+  google::InitGoogleLogging("romp");
+  LOG(INFO) << "start initializing ompt";      
+
+}
+
+/**
+ *  release resources and log info upon finalization of tool
+ */
+void omptFinalize(ompt_data_t* toolData) {
+  LOG(INFO) << "finalizing ompt";
+}
+
+/** 
+ * implement ompt_start_tool which is defined in OpenMP spec 5.0
+ */
+ompt_start_tool_result_t* ompt_start_tool(
+        unsigned int ompVersion,
+        const char* runtimeVersion) {
+  ompt_data_t data;
+  static ompt_start_tool_result_t startToolResult = { 
+      &omptInitialize, &omptFinalize, data}; 
+  LOG(INFO) << "ompt_start_tool";
+  return &startToolResult;
+}
 
 
 
