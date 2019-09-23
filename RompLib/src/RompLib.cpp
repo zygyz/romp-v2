f7088abd68285b40c036c6ea68f4dcea910f5eb7
Yizi Gu <yizigu@gmail.com>
2019-09-22 21:53:38 -0500

    [refactor][romp-lib] try make ompt_start_tool work

--- RompLib/src/RompLib.cpp
+++ RompLib/src/RompLib.cpp
@@ -1,9 +1,34 @@
-#include "Initialize.h"
+//#include "Initialize.h"
+
+#include <glog/logging.h>
+#include <ompt.h>
 
 
 using namespace std;
 
 namespace romp {
+
+int omptInitialize(ompt_function_lookup_t functionLookup,
+                   int initialDeviceNum,
+                   ompt_data_t* toolData) {
+  google::InitGoogleLogging("romp");
+  LOG(INFO) << "start initializing ompt";      
+
+}
+
+void omptFinalize(ompt_data_t* toolData) {
+  LOG(INFO) << "finalizing ompt";
+}
+
+ompt_start_tool_result_t* ompt_start_tool(
+        unsigned int ompVersion,
+        const char* runtimeVersion) {
+  ompt_data_t data;
+  static ompt_start_tool_result_t startToolResult = { 
+      &omptInitialize, &omptFinalize, data}; 
+  LOG(INFO) << "ompt_start_tool";
+  return &startToolResult;
+}
 
 extern "C" {
 
