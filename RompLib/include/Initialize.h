2b94ab7f603121e32bc54f0059ebaf2ca3da5e91
Yizi Gu <yizigu@gmail.com>
2019-09-24 18:54:29 -0500

    [refactor][romp-lib] register ompt callback

--- RompLib/include/Initialize.h
+++ RompLib/include/Initialize.h
@@ -1,12 +1,24 @@
 #pragma once
-
-#include <glog/logging.h>
-#include <ompt.h>
-
-/* This header file defines functions that are used 
+#include "Callbacks.h"
+/* 
+ * This header file defines functions that are used 
  * to initialize OMPT interface. 
 */
 namespace romp{
+
+/* 
+ * Define macro for registering ompt callback functions. 
+ */
+#define register_callback(name, type)                        \
+do {                                                         \
+  type f_##name = &on_##name;                                \
+  if (ompt_set_callback(name, (ompt_callback_t)f_##name) ==  \
+      ompt_set_never)                                        \
+    LOG(ERROR) << "Could not register callback";             \
+} while(0)
+
+#define register_callback(name) register_callback_t(name, name##_t)
+
 /** 
  *  initialize OMPT interface by registering callback functions
  */
@@ -15,6 +27,9 @@ int omptInitialize(ompt_function_lookup_t functionLookup,
                    ompt_data_t* toolData) {
   google::InitGoogleLogging("romp");
   LOG(INFO) << "start initializing ompt";      
+  auto ompt_set_callback = (ompt_set_callback_t)lookup("ompt_set_callback");
+  register_callback(ompt_callback_implicit_task);
+  return 1;
 
 }
 
