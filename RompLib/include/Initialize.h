18dfee00a21a807d763fdbf5e4fe001d9a73a933
Yizi Gu <yizigu@gmail.com>
2019-09-24 00:21:16 -0500

    [refactor][romp-lib] again :P, move the ompt initialization to header file

--- RompLib/include/Initialize.h
+++ RompLib/include/Initialize.h
@@ -7,7 +7,40 @@
  * to initialize OMPT interface. 
 */
 namespace romp{
+/** 
+ *  initialize OMPT interface by registering callback functions
+ */
+int omptInitialize(ompt_function_lookup_t functionLookup,
+                   int initialDeviceNum,
+                   ompt_data_t* toolData) {
+  google::InitGoogleLogging("romp");
+  LOG(INFO) << "start initializing ompt";      
+  cout << "start initializing ompt" << endl;
 
+}
+
+/**
+ *  release resources and log info upon finalization of tool
+ */
+void omptFinalize(ompt_data_t* toolData) {
+  LOG(INFO) << "finalizing ompt";
+  cout << "finalizing ompt" << endl;
+}
+
+
+/** 
+ * implement ompt_start_tool which is defined in OpenMP spec 5.0
+ */
+ompt_start_tool_result_t* ompt_start_tool(
+        unsigned int ompVersion,
+        const char* runtimeVersion) {
+  ompt_data_t data;
+  static ompt_start_tool_result_t startToolResult = { 
+      &omptInitialize, &omptFinalize, data}; 
+  LOG(INFO) << "ompt_start_tool";
+  cout << "ompt_start_tool" << endl;
+  return &startToolResult;
+}
 
 }
 
