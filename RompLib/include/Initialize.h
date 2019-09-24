ed81974642248c387ac8116a0f8ee7f40e28ed95
Yizi Gu <yizigu@gmail.com>
2019-09-24 00:41:21 -0500

    [debug] move ompt_start_tool to cpp?

--- RompLib/include/Initialize.h
+++ RompLib/include/Initialize.h
@@ -26,18 +26,5 @@ void omptFinalize(ompt_data_t* toolData) {
 }
 
 
-/** 
- * implement ompt_start_tool which is defined in OpenMP spec 5.0
- */
-ompt_start_tool_result_t* ompt_start_tool(
-        unsigned int ompVersion,
-        const char* runtimeVersion) {
-  ompt_data_t data;
-  static ompt_start_tool_result_t startToolResult = { 
-      &omptInitialize, &omptFinalize, data}; 
-  LOG(INFO) << "ompt_start_tool";
-  return &startToolResult;
-}
-
 }
 
--- RompLib/src/RompLib.cpp
+++ RompLib/src/RompLib.cpp
@@ -10,6 +10,19 @@ namespace romp {
 
 extern "C" {
 
+/** 
+ * implement ompt_start_tool which is defined in OpenMP spec 5.0
+ */
+ompt_start_tool_result_t* ompt_start_tool(
+        unsigned int ompVersion,
+        const char* runtimeVersion) {
+  ompt_data_t data;
+  static ompt_start_tool_result_t startToolResult = { 
+      &omptInitialize, &omptFinalize, data}; 
+  LOG(INFO) << "ompt_start_tool";
+  return &startToolResult;
+}
+
 
 void 
 checkAccess(void* address,
