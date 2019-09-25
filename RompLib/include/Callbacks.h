f8554791e17d06cc8ad7e72a394f4672382134a8
Yizi Gu <yizigu@gmail.com>
2019-09-25 02:25:53 -0500

    [refactor][romp-lib] added on_ompt_callback_sync_region callback

--- RompLib/include/Callbacks.h
+++ RompLib/include/Callbacks.h
@@ -14,4 +14,12 @@ void on_ompt_callback_implicit_task(
         unsigned int actualParallelism,
         unsigned int index,
         int flags);
+
+void on_ompt_callback_sync_region(
+        ompt_sync_region_t kind,
+        ompt_scope_endpoint_t endPoint,
+        ompt_data_t *parallelData,
+        ompt_data_t *taskData,
+        const void* codePtrRa); 
+
 }
--- RompLib/src/Callbacks.cpp
+++ RompLib/src/Callbacks.cpp
@@ -12,4 +12,14 @@ void on_ompt_callback_implicit_task(
   LOG(INFO) << "on_ompt_callback_implicit_task called"; 
 }
 
+void on_ompt_callback_sync_region(
+       ompt_sync_region_t kind,
+       ompt_scope_endpoint_t endPoint,
+       ompt_data_t *parallelData,
+       ompt_data_t *taskData,
+       const void* codePtrRa) {
+  LOG(INFO) << "on_ompt_callback_sync_region called";
+
+}
+
 }
