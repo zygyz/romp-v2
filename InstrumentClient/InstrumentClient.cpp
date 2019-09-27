012c04dfbb7cee33870df66a3ed1d355aa22635e
Yizi Gu <yizigu@gmail.com>
2019-09-27 17:25:12 -0500

    [debug] not skipping libpthread cause segment fault

--- InstrumentClient/InstrumentClient.cpp
+++ InstrumentClient/InstrumentClient.cpp
@@ -83,7 +83,7 @@ InstrumentClient::getFunctionsVector(
   }
   char nameBuffer[MODULE_NAME_LENGTH];
   vector<string> skipLibraryName = { "libc.so.6", 
-                                     "libpthread.so.0",
+                                   //  "libpthread.so.0",
                                      "libgcc_s.so.1",
                                    //  "libgomp.so.1", 
                                     // "libm.so.6",
