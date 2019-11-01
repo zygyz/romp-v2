68f34f57a5db5cbe21a260fa43fcfa347a26a91f
Yizi Gu <yizigu@gmail.com>
2019-10-31 21:56:48 -0500

    [refactor][romp-lib] initialize unique_ptr to vector of records

--- RompLib/include/AccessHistory.h
+++ RompLib/include/AccessHistory.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 #include "Record.h"
@@ -9,10 +10,14 @@ namespace romp {
 class AccessHistory {
 
 public: 
-  AccessHistory() : state(0) {}
+  AccessHistory() : state(0) {  
+    _records.reset(new std::vector<Record>()); // be c++11 compatible 
+  }
   void setState(uint8_t state);
 private:
   uint8_t state;  
+  std::unique_ptr<std::vector<Record>> _records; 
+
 };
 
 }
