6c3c992ebadb5c42e6ecc2e45531b336360986f4
Yizi Gu <yizigu@gmail.com>
2019-11-04 11:48:14 -0600

    [refactor][romp-lib] added Segment.h/cpp

---
+++ RompLib/include/Segment.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <cstdint>
+#include <memory> 
+#include <string>
+
+namespace romp {
+
+enum SegmentType {
+  eImplicit = 0x1,
+  eExplicit = 0x2,
+  eWorkShare = 0x3, 
+};
+/*
+ *  The abstract class definition for label segment 
+ */
+class Segment {
+public: 
+  virtual std::string toString() const = 0;
+  virtual void setType(SegmentType type) = 0; 
+  virtual SegmentType getType() const = 0;
+  virtual std::shared_ptr<Segment> clone() const = 0;  
+  virtual void setOffsetSpan(uint64_t offset, uint64_t span) = 0;
+  virtual void getOffsetSpan(uint64_t& offset, uint64_t& span) const = 0;
+  virtual void setTaskwait(uint64_t taskwait) = 0;
+  virtual void getTaskwait(uint64_t& taskwait) const = 0;
+  virtual void setPhase(uint64_t phase) = 0;
+  virtual void getPhase(uint64_t& phase) const = 0;
+  virtual void setLoopCount(uint64_t loopCount) = 0;
+  virtual void getLoopCount(uint64_t& loopCount) const = 0;  
+  virtual ~Segment() = default;
+};
+
+/*
+ * Base segment is for representing implicit task with no 
+ * worksharing construct attached to it.
+ */
+class BaseSegment : public Segment {
+public:
+  BaseSegment(): value(0) {}
+  BaseSegment(const BaseSegment& segment): value(segment.value) {}
+  BaseSegment(SegmentType type, uint64_t offset, uint64_t span);
+  std::string toString() const override;
+  void setType(SegmentType type) override;
+  SegmentType getType() const override;
+  std::shared_ptr<Segment> clone() const override;
+  void setOffsetSpan(uint64_t offset, uint64_t span) override;
+  void getOffsetSpan(uint64_t& offset, uint64_t& span) const override;
+  void setTaskwait(uint64_t taskwait) override;
+  void getTaskwait(uint64_t& taskwait) const override;
+  void setPhase(uint64_t phase) override;
+  void getPhase(uint64_t& phase) const override;
+  void setLoopCount(uint64_t loopCount) override;
+  void getLoopCount(uint64_t& loopCount) const override;
+protected:
+  uint64_t value;
+};
+
+/*
+ * Workshare segment is for representing task with worksharing 
+ * construct.
+ */
+class WorkShareSegment: public BaseSegment {
+public:
+  WorkShareSegment() : workShareId(0) { setType(eWorkShare); }
+  WorkShareSegment(uint64_t id, bool isSection): workShareId(id) { 
+    setType(eWorkShare); 
+    setWorkShareType(isSection);
+  } 
+  WorkShareSegment(const WorkShareSegment& segment): BaseSegment(segment), 
+     workShareId(segment.workShareId) { }                       
+  void setPlaceHolderFlag(bool toggle);
+  bool isPlaceHolder() const;
+  void setWorkShareType(bool isSection);
+  bool isSection() const;
+  void setSingleFlag(bool isExecutor);
+  bool isSingleExecutor() const;
+  bool isSingleOther() const;
+  std::string toString() const override;
+  std::shared_ptr<Segment> clone() const override;
+private: 
+  uint64_t workShareId; 
+};
+
+
+}
---
+++ RompLib/src/Segment.cpp
@@ -0,0 +1,194 @@
+#include "Segment.h"
+
+#include <glog/logging.h>
+#include <glog/raw_logging.h>
+#include <sstream>
+
+#define SEG_TYPE_MASK        0x0000000000000003
+#define OFFSET_MASK          0xffff000000000000
+#define SPAN_MASK            0x0000ffff00000000
+#define TASKWAIT_MASK        0x00000000f0000000
+#define PHASE_MASK           0x000000000f000000
+#define WS_PLACE_HOLDER_MASK 0xfffffffffffffffb
+#define LOOP_CNT_MASK        0x0000000000f00000
+#define SINGLE_MASK          0xc000000000000000
+#define WORKSHARE_TYPE_MASK  0x0000000000000004
+
+#define OFFSET_SPAN_WIDTH 16
+
+#define OFFSET_SHIFT 48
+#define SPAN_SHIFT 32
+#define TASKWAIT_SHIFT 28
+#define PHASE_SHIFT 24
+#define LOOP_CNT_SHIFT 20
+#define WS_PLACE_HOLDER_POS 2  // least significant bit index is 0
+#define SINGLE_EXEC_SHIFT 63
+#define SINGLE_OTHER_SHIFT 62
+
+
+namespace romp {
+
+/*
+ * Each segment contains a 64 bit value. From low to high, assign index 0-63
+ * [0,1]: segment type 
+ * [48, 63]: offset 
+ * [32, 47]: span 
+ * [43, 46]: taskwait count
+ * [39, 42]: phase count
+ * [35, 38]: loop count
+ * [2]: mark if current workshare semgent is section, bit set: yes. 
+ *      otherwise, sgment is iteration
+ *
+ * For workshare segment, we use the extra workShareId to store information
+ * [0,31]: work share id
+ * [62,63]: single construct flag bits 
+ */
+std::string BaseSegment::toString() const {
+  std::stringstream stream;
+  stream << std::hex << value;
+  auto result = "[" + stream.str() + "]";
+  return result;
+}
+
+BaseSegment::BaseSegment(SegmentType type, uint64_t offset, 
+        uint64_t span) {
+  if (span >= (1 << OFFSET_SPAN_WIDTH)) {
+    RAW_LOG(FATAL, "%s %lu", "span is overflowing", span);
+  }
+  value = 0;
+  setType(type);
+  setOffsetSpan(offset, span);
+}
+
+std::shared_ptr<Segment> BaseSegment::clone() const {
+  return std::make_shared<BaseSegment>(*this);
+}
+
+void BaseSegment::setOffsetSpan(uint64_t offset, uint64_t span) {
+  value &= ~(OFFSET_MASK | SPAN_MASK);  // clear the offset, span field
+  value |= (offset << OFFSET_SHIFT) & OFFSET_MASK; 
+  value |= (span << SPAN_SHIFT) & SPAN_MASK; 
+}
+
+void BaseSegment::getOffsetSpan(uint64_t& offset, uint64_t& span) const {
+  offset = (value & OFFSET_MASK) >> OFFSET_SHIFT;
+  span = (value & SPAN_MASK) >> SPAN_SHIFT;
+}
+
+/*
+ * Taskwait field is four bits. So if taskwait is more than 16, it overflows.
+ */
+void BaseSegment::setTaskwait(uint64_t taskwait) {
+  if (taskwait > 16) {
+    RAW_LOG(FATAL, "taskwait count is overflowing %lu", taskwait);
+  }
+  value &= ~TASKWAIT_MASK; // clear the taskwait field
+  value |= (taskwait << TASKWAIT_SHIFT) & TASKWAIT_MASK;
+}
+
+void BaseSegment::getTaskwait(uint64_t& taskwait) const {
+  taskwait = (value & TASKWAIT_MASK) >> TASKWAIT_SHIFT;
+}
+
+void BaseSegment::setPhase(uint64_t phase) {
+  if (phase > 16) {
+    RAW_LOG(FATAL, "phase count is overflowing %lu", phase);
+  }
+  value &= ~PHASE_MASK;
+  value |= (phase << PHASE_SHIFT) & PHASE_MASK;
+}
+
+void BaseSegment::getPhase(uint64_t& phase) const {
+  phase = (value & PHASE_MASK) >> PHASE_SHIFT;
+}
+
+void BaseSegment::setLoopCount(uint64_t loopCount) {
+  if (loopCount > 16) {
+    RAW_LOG(FATAL, "loop count is overflowing %lu", loopCount);
+  }    
+  value &= ~LOOP_CNT_MASK;
+  value |= (loopCount << LOOP_CNT_SHIFT) & LOOP_CNT_MASK;
+}
+
+void BaseSegment::getLoopCount(uint64_t& loopCount) const {
+  loopCount = (value & LOOP_CNT_MASK) >> LOOP_CNT_SHIFT;
+}
+
+void BaseSegment::setType(SegmentType type) {
+  value |= static_cast<uint64_t>(type);
+}
+
+SegmentType BaseSegment::getType() const {
+  auto mask = value & SEG_TYPE_MASK;
+  switch(mask) {
+    case 0x1:
+      return eImplicit;
+    case 0x2:
+      return eExplicit;
+    case 0x3:
+      return eWorkShare; 
+  }
+  RAW_LOG(FATAL, "undefined segment type: %d", mask);
+}
+
+std::string WorkShareSegment::toString() const {
+  std::stringstream stream;
+  stream << std::hex << value;
+  auto result = "[" + stream.str() + "," + 
+      std::to_string(workShareId) + "]";
+  return result;
+}
+
+std::shared_ptr<Segment> WorkShareSegment::clone() const {
+  return std::make_shared<WorkShareSegment>(*this);
+}
+
+/*
+ * Set place holder flag for the workshare segment. If toggle is true,
+ * set the flag, otherwise, clear the flag.
+ */
+void WorkShareSegment::setPlaceHolderFlag(bool toggle) {
+  if (toggle) {
+    value |= (1 << WS_PLACE_HOLDER_POS);
+  } else {
+    value &= WS_PLACE_HOLDER_MASK;
+  }
+}
+
+bool WorkShareSegment::isPlaceHolder() const {
+  return ((value & WS_PLACE_HOLDER_MASK) >> WS_PLACE_HOLDER_POS) == 1;
+}
+
+bool WorkShareSegment::isSingleExecutor() const {
+  return ((workShareId & SINGLE_MASK) >> SINGLE_EXEC_SHIFT) == 1;
+}
+
+bool WorkShareSegment::isSingleOther() const {
+  return ((workShareId & SINGLE_MASK) >> SINGLE_OTHER_SHIFT) == 1;
+}
+
+void WorkShareSegment::setSingleFlag(bool isExecutor) {
+  workShareId &= ~SINGLE_MASK;
+  uint64_t b = 1;
+  if (isExecutor) {
+    // toggle the higher bit to 1
+    workShareId |= (b << SINGLE_EXEC_SHIFT);
+  } else {
+    // single other, toggle the lower bit to 1
+    workShareId |= (b << SINGLE_OTHER_SHIFT);
+  }
+}
+
+void WorkShareSegment::setWorkShareType(bool isSection) {
+  value &= ~WORKSHARE_TYPE_MASK; // clear the bit first
+  if (isSection) {
+    value |= WORKSHARE_TYPE_MASK;  // set the bit
+  } 
+}
+
+bool WorkShareSegment::isSection() const {
+  return (value & WORKSHARE_TYPE_MASK) != 0;
+}
+
+
+}
