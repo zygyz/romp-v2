0e7c36376ac79449a74d34627002c9f4f3078dd6
Yizi Gu <yizigu@gmail.com>
2020-01-14 17:45:29 -0600

    [debug] removed logging for debug

--- RompLib/src/DataSharing.cpp
+++ RompLib/src/DataSharing.cpp
@@ -41,8 +41,6 @@ DataSharingType analyzeDataSharing(const void* threadDataPtr,
     RAW_LOG(INFO, "thread stack bound is not completely set");
     return eUndefined;
   }
-  RAW_LOG(INFO, "stack base: %lx top: %lx address: %lx", stackBaseAddr, stackTopAddr, 
-          address);
   const auto addressValue = reinterpret_cast<const uint64_t>(address);
   if (addressValue >= reinterpret_cast<const uint64_t>(
                   STATIC_THREAD_PRIVATE_LOWER_BOUND)) {
