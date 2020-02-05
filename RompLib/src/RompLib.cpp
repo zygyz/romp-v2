34ebd8fd50dd7c74e21c5750ff55d591ba93a245
Yizi Gu <yizigu@gmail.com>
2020-02-05 13:04:52 -0600

    [misc] adjusted comments

--- RompLib/src/RompLib.cpp
+++ RompLib/src/RompLib.cpp
@@ -46,7 +46,6 @@ void checkDataRace(AccessHistory* accessHistory, const LabelPtr& curLabel,
      * race is reported, romp clears the access history with respect to this
      * memory location and mark this memory location as found. Future access 
      * to this memory location does not go through data race checking.
-     * TODO: implement the logic described above.
      */
     if (!records->empty()) {
       records->clear();
